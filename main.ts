e0363de6c46970cacfdcc47bc6ac6058c4c25efe
Derv6464 <36761419+Derv6464@users.noreply.github.com>
2021-05-25 11:07:25 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,36 @@
-basic.forever(function () {
-	
+function long2 () {
+    basic.showString("L")
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    for (let index = 0; index < 12; index++) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 25)
+        basic.pause(100)
+    }
+    for (let index = 0; index < 12; index++) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 25)
+        basic.pause(100)
+    }
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+    basic.clearScreen()
+}
+input.onButtonPressed(Button.A, function () {
+    long2()
+})
+function short2 () {
+    basic.showString("s")
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    for (let index = 0; index < 4; index++) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
+        basic.pause(100)
+    }
+    for (let index = 0; index < 4; index++) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 100)
+        basic.pause(100)
+    }
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+    basic.clearScreen()
+}
+input.onButtonPressed(Button.B, function () {
+    short2()
 })
